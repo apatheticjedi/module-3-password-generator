744ee86b2f5d175e3c52439465431ef19a47dd57
Duder <thedudeabides1981@hotmail.com>
2022-07-30 16:32:57 -0700

    added prompts for password criteria

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // PSEUDOCODE:
 // Click a button to generate a password
-// Prompt user for criteria including: password length, include lowercase, include uppercase, include numeric, include special characters
+// Prompt user for criteria including: password length, include lowercase, include uppercase, include numbers, include special characters
 // Validate user choices and include at least one of each character type
 // Generate password
 // Display password in provided text box
@@ -8,9 +8,94 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// strings with possible password characters
+const options = {
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*_-+="
+};
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  // validate and return length of password
+  var getLength = function () {
+    var length = window.prompt("How long would you like to make your password (8-128 characters)?");
+    if (length === null) {
+      window.alert("Please enter a number.");
+      getLength();
+    } else if (isNaN(length)) {
+      window.alert("please enter a valid number.");
+      getLength();
+    } else if (length < 8 || length > 128) {
+      window.alert("Please enter a valid length.");
+      getLength();
+    } else {
+      return length;
+    }
+  };
+
+  // validate and return if password will include lowercase letters
+  var lowerCase = function () {
+    lowerCase = window.confirm("Include lowercase letters?");
+    if (!lowerCase) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // validate and return if password will return uppercase letters
+  var upperCase = function () {
+    upperCase = window.confirm("Include uppercase letters?");
+    if (!upperCase) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // validate and return if password will include numbers
+  var numInclude = function () {
+    numInclude = window.confirm("Include numbers?");
+    if (!numInclude) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // validate and return if password will include special characters
+  var specialChar = function () {
+    specialChar = window.confirm("Include special characters (!,#,&,%...)?");
+    if (!specialChar) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // password parameters as requested by user
+  var userInput = {
+    length: getLength(),
+    lower: lowerCase(),
+    upper: upperCase(),
+    number: numInclude(),
+    special: specialChar()
+  };
+
+  // log user created password variables
+  console.log(userInput.length);
+  console.log(userInput.lower);
+  console.log(userInput.upper);
+  console.log(userInput.number);
+  console.log(userInput.special);
+
+  /* var password = generatePassword() {
+
+  }; */
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
